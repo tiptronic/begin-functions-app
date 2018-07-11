81fae5f232b2b6d5a6cac27a2bd6f2a027c08d9f
Andy Fuchs <tiptronic@me.com>
2018-07-11 10:28:20 +0200

    Peronalize

--- src/html/get-index/index.js
+++ src/html/get-index/index.js
@@ -3,7 +3,7 @@ let begin = require('@architect/functions')
 function route(req, res) {
   console.log(JSON.stringify(req, null, 2))
   res({
-    html: 'Hello world!'
+    html: 'Hello Andy!'
   })
 }
 
